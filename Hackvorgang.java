fdb423c8b657634f4f6fafc2af7623460fa3c153
crasbe <crasbe@googlemail.com>
2015-06-14 20:04:53 +0200

    Create Hackvorgang.java

---
+++ Hackvorgang.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+
+public abstract class Hackvorgang {
+
+	Properties prop = new Properties();
+	InputStream input = null;
+	
+	private void ladeHack() {
+		
+	}
+	
+}
