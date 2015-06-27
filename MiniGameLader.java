90a2c95af90704a6b7731d791e89cb5ffe17e262
crasbe <crasbe@googlemail.com>
2015-06-27 03:14:40 +0200

    Update MiniGameLader.java

--- MiniGameLader.java
+++ MiniGameLader.java
@@ -1,38 +1,26 @@
 import java.util.*;
 
-
 public class MiniGameLader {
 	
 	// in miniGames werden alle initialisierten MiniGames gespeichert
-	private List<AbsractMiniGame> miniGames = new ArrayList<AbstractMiniGame>();
+	private List<AbstractMiniGame> miniGames = new ArrayList<AbstractMiniGame>();
 	
 	private Random rand = new Random();
-	
-	public MiniGameLader() {
+
+	public MiniGameLader(int schwierigkeit) {
 		// die Objekte der Minigames werden hier initialisiert und dem
 		// Array hinzugefügt.
 		// (Mit Arrays arbeiten zu wollen kommt von Python :))
-		miniGames.add(new TestGame());
+		//miniGames.add(new TestGame(schwierigkeit));
+		miniGames.add(new ReflKey(schwierigkeit));
 		
 	}
 	
-	public AbstactMiniGame miniGameWaehlen(int schwierigkeit) {
+	public AbstractMiniGame miniGameWaehlen() {
 		// diese Methode gibt ein zufälliges MiniGame-Objekt zurück
-		// welches nach der Schwierigkeit bestimmt wird
 		
-		// dieses Attribut speichert die MiniGames, die nach der
-		// Schwierigkeit her in Frage kommen
-		List<AbstractMiniGame> ausgewaehlteMiniGames = new ArrayList<AbstractMiniGame>();
+		int randomGame = rand.nextInt(miniGames.size());
 		
-		for(int i = miniGames.size(); i > 0; i--) {
-			if(miniGames.get(i).schwierigkeit == schwierigkeit) {
-				ausgewaehlteMiniGames.add(miniGames.get(i));
-			}
-		}
-		
-		int randomGame = rand.nextInt(ausgewaehlteMiniGames.size());
-		
-		return ausgewaehlteMiniGames.get(randomGame);
+		return miniGames.get(randomGame);
 	}
-
 }
