724c1241a279bd85116fa594a2a075ef6e4c99dc
crasbe <crasbe@googlemail.com>
2015-07-01 19:50:00 +0200

    Update ReflKey.java
    ReflKey.java an die Konventionen des AbstractMiniGame angepasst und aufgeräumt

--- ReflKey.java
+++ ReflKey.java
@@ -1,19 +1,22 @@
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
+
+import java.util.Random;
+import java.util.Arrays;
+import java.util.List;
 
 public class ReflKey extends AbstractMiniGame {
 	// Anfang Attribut
 	private static String titel = "ReflKey";
-	
+
 	private int kill = 0;
 	private int eingabe;
 	private int code;
-	private int erg1 = (int) ((Math.random() * 9) + 0);
-	private int erg2 = (int) ((Math.random() * 9) + 0);
-	private int erg3 = (int) ((Math.random() * 9) + 0);
-	private int erg4 = (int) ((Math.random() * 9) + 0);
-	private int erg5 = (int) ((Math.random() * 9) + 0);
+	private int durchgaenge; // die Anzahl an Durchgängen im Text
+	private Random rand = new Random();
+	
 	private JTextField txtfueberschrift = new JTextField();
 	private JButton decstart = new JButton();
 	private JTextField nrfErgebnis = new JTextField();
@@ -22,29 +25,25 @@ public class ReflKey extends AbstractMiniGame {
 	private JTextField zahl3 = new JTextField();
 	private JTextField zahl4 = new JTextField();
 	private JTextField zahl5 = new JTextField();
-	private JLabel jLabel1 = new JLabel();
 	private JTextField ueberschrift2 = new JTextField();
+	private JTextField achtung = new JTextField();
 
-	private int durchgaenge = 0;
-	private String[] text1 = { " Q", " " + erg1, " K", " ?", " §", " A", " &",
-			" C" };
-	private String[] text2 = { " H", " $", " M", " " + erg2, " %", " S", " =",
-			" P" };
-	private String[] text3 = { " B", " %", " Y", " !", " N", " !", " " + erg3,
-			" F" };
-	private String[] text4 = { " L", " (", " " + erg4, " /", " K", " =", " V",
-			" §" };
-	private String[] text5 = { " X", " !", " D", " &", " " + erg5, " ?", " T",
-			" $" };
+	// variable Elemente: 1+0, 3+8, 6+16, 2+24, 4+32
+	//                    1,   11,  22,   26,   36
+	private List<String> text = Arrays.asList(	
+			" Q", "", " K", " ?", " §", " A", " &", " C",
+			" H", " $", " M", " ", " %", " S", " =", " P",
+			" B", " %", " Y", " !",	 " N", " !", " ", " F",
+			" L", " (", " ", " /", " K", " =", " V", " §",
+			" X", " !", " D", " &", " ", " ?", " T", " $" );
 
 	private ActionListener buchAnz = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-			System.out.println(""+erg1+" "+erg2+" "+erg3+" "+erg4+" "+erg5);
-			zahl1.setText(text1[durchgaenge]);
-			zahl2.setText(text2[durchgaenge]);
-			zahl3.setText(text3[durchgaenge]);
-			zahl4.setText(text4[durchgaenge]);
-			zahl5.setText(text5[durchgaenge]);
+			zahl1.setText(text.get(durchgaenge+0));
+			zahl2.setText(text.get(durchgaenge+8));
+			zahl3.setText(text.get(durchgaenge+16));
+			zahl4.setText(text.get(durchgaenge+24));
+			zahl5.setText(text.get(durchgaenge+32));
 			durchgaenge++;
 			if (durchgaenge == 8) {
 				tm.stop();
@@ -53,7 +52,6 @@ public class ReflKey extends AbstractMiniGame {
 	};
 
 	private Timer tm = new Timer(1000, buchAnz);
-	private JTextField achtung = new JTextField();
 
 	// Ende Attribute
 
@@ -163,44 +161,72 @@ public class ReflKey extends AbstractMiniGame {
 	// Anfang Methoden
 
 	public static void main(String[] args) {
-		new ReflKey(0).setVisible(true);;
-	} // end of main
-
-	public void nrfErgebnis_ActionPerformed(ActionEvent evt) {
-		if (evt.getSource().equals(nrfErgebnis)) {
-			code = erg1 * 10000 + erg2 * 1000 + erg3 * 100 + erg4 * 10 + erg5;
-			eingabe = Integer.parseInt(nrfErgebnis.getText());
-			if (code == eingabe) {
-				achtung.setVisible(true);
-				achtung.setText("  Hackversuch Erfolgreich!");
-				this.erfolg = true;
-			} // end of if
-			else {
-				achtung.setVisible(true);
-				achtung.setText("  Passwort Fehlerhaft.");
-				this.erfolg = false;
-			} // end of if-else
-			
-			this.versuche = 1;
-			this.fertig = true;
-		} // end of if
+		AbstractMiniGame reflKey = new ReflKey(0);
+		reflKey.initialisieren();
+		reflKey.setVisible(true);
 	}
 
+	private void nrfErgebnis_ActionPerformed(ActionEvent evt) {
+		// die Eingabe des Spielers ins Integer-Format bringen
+		eingabe = Integer.parseInt(nrfErgebnis.getText());
+		
+		// das 
+		if(code == eingabe) {
+			achtung.setText("  Hackversuch erfolgreich!");
+			this.erfolg = true;
+			this.fertig = true;
+			this.versuche = kill;
+		}
+		else {
+			achtung.setText("  Passwort fehlerhaft.");
+		}
+		// das Hinweisfeld sichtbar machen
+		achtung.setVisible(true);
+	}
+	
 	public void decstart_MousePressed(MouseEvent evt) {
-		if (kill <= 6) {
+		kill++;
+		
+		// TODO: von der gegebenen Schwierigkeit abhängig machen
+		if(kill <= 6) { // 6 mal darf man auf den Decoder-Button drücken
 			durchgaenge = 0;
 			tm.setInitialDelay(0);
 			tm.start();
-		} // end of if
+		}
 		else {
 			achtung.setVisible(true);
-			achtung.setText("     Hack Misslungen.");
-		} // end of if-else
-		kill = kill + 1;
-		if (kill == 6) {
+			achtung.setText("     Hack misslungen.");
+			this.erfolg = false;
+			this.fertig = true;
+		}
+		if(kill == 6) {
 			achtung.setVisible(true);
 			achtung.setText("   WARNUNG: Last Try.");
-		} // end of if
+		}
 	}
-	// Ende Methoden
+
+	public void initialisieren() {
+		// hier werden alle Zufallszahlen neu generiert und der
+		// Code daraus gebildet.
+		int erg1 = rand.nextInt(10);
+		int erg2 = rand.nextInt(10);
+		int erg3 = rand.nextInt(10);
+		int erg4 = rand.nextInt(10);
+		int erg5 = rand.nextInt(10);
+		
+		text.set(1+0,  " "+erg1); // das Textarray wird mit den Zahlen gefüllt
+		text.set(3+8,  " "+erg2);
+		text.set(6+16, " "+erg3);
+		text.set(2+24, " "+erg4);
+		text.set(4+32, " "+erg5);
+		
+		code = erg1 * 10000 + erg2 * 1000 + erg3 * 100 + erg4 * 10 + erg5;
+		System.out.println(code);
+		
+		// zurück auf Start :)
+		durchgaenge = 0;
+		versuche = 0;
+		fertig = false;
+		erfolg = false;
+	}
 }
