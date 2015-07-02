0e7c2d803bbad525c37369123984eb87560f6c46
crasbe <crasbe@googlemail.com>
2015-07-02 20:02:42 +0200

    Update WieZu.java
    Neustarbar gemacht und an MiniGame-Layout angepasst

--- WieZu.java
+++ WieZu.java
@@ -1,214 +1,181 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.*;
+
 import java.util.*;
-/**
-  *
-  * Beschreibung
-  *
-  * @version 1.0 vom 23.06.2015
-  * @author 
-  */
+import java.util.List;
+import java.util.ArrayList;
 
 public class WieZu extends AbstractMiniGame {
-  // Anfang Attribute
- private class QuizLader extends AbstractLader {
-    public QuizLader() {
-      super ("quiz");
-    }
-  }  
-  // DummyKlasse  
-  // DummyKlasse                                 
-  private JRadioButton antw2 = new JRadioButton();
-  private JRadioButton antw3 = new JRadioButton();
-  private JRadioButton antw4 = new JRadioButton();
-  private ButtonGroup antwGroup = new ButtonGroup();
-  private JRadioButton antw1 = new JRadioButton();
-  private JTextField aufgabe = new JTextField();
-  private int antw;
-  private String richt;
-  private int pktCnt = 0;
-  private int rndCnt = 1;
-  private AbstractLader quizLader = new QuizLader();
-  private Properties quizProp = quizLader.getProps().get(0);
-  private String auswahl = "";
-  private JButton btnAbschicken = new JButton();
-  private MiniGamesWinScreen WinScreen;
-  private MiniGamesLoseScreen LoseScreen;
-  // Ende Attribute
-  
-  public WieZu(String title) { 
-    // Frame-Initialisierung
-    super(title);
-    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    int frameWidth = 457; 
-    int frameHeight = 386;
-    setSize(frameWidth, frameHeight);
-    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-    int x = (d.width - getSize().width) / 2;
-    int y = (d.height - getSize().height) / 2;
-    setLocation(x, y);
-    setResizable(false);
-    Container cp = getContentPane();
-    cp.setLayout(null);
-    // Anfang Komponenten
-    
-    antw2.setBounds(56, 176, 233, 41);
-    antw2.setText("");
-    antw2.setActionCommand("a2");
-    antw2.setOpaque(true);
-    antw2.addActionListener(new ActionListener() { 
-      public void actionPerformed(ActionEvent evt) { 
-        antw2_ActionPerformed(evt);
-      }
-    });
-    antw2.setBackground(Color.BLACK);
-    antw2.setForeground(Color.GREEN);
-    antw2.setFont(new Font("Fixedsys", Font.PLAIN, 12));
-    antw2.setCursor(new Cursor(Cursor.HAND_CURSOR));
-    cp.add(antw2);
-    antw3.setBounds(56, 224, 241, 41);
-    antw3.setText("");
-    antw3.setActionCommand("a3");
-    
-    antw3.setOpaque(true);
-    antw3.addActionListener(new ActionListener() { 
-      public void actionPerformed(ActionEvent evt) { 
-        antw3_ActionPerformed(evt);
-      }
-    });
-    antw3.setBackground(Color.BLACK);
-    antw3.setFont(new Font("Fixedsys", Font.PLAIN, 12));
-    antw3.setForeground(Color.GREEN);
-    antw3.setCursor(new Cursor(Cursor.HAND_CURSOR));
-    cp.add(antw3);
-    antw4.setBounds(56, 272, 183, 41);
-    antw4.setText("");
-    antw4.setActionCommand("a4");
-    antw4.setOpaque(true);
-    antw4.addActionListener(new ActionListener() { 
-      public void actionPerformed(ActionEvent evt) { 
-        antw4_ActionPerformed(evt);
-      }
-    });
-    antw4.setBackground(Color.BLACK);
-    antw4.setForeground(Color.GREEN);
-    antw4.setFont(new Font("Fixedsys", Font.PLAIN, 12));
-    antw4.setCursor(new Cursor(Cursor.HAND_CURSOR));
-    cp.add(antw4);
-    antw1.setBounds(56, 128, 233, 41);
-    antw1.setText("");
-    antw1.setActionCommand("a1");
-    antw1.setOpaque(true);
-    antw1.addActionListener(new ActionListener() { 
-      public void actionPerformed(ActionEvent evt) { 
-        antw1_ActionPerformed(evt);
-      }
-    });
-    antw1.setBackground(Color.BLACK);
-    antw1.setForeground(Color.GREEN);
-    antw1.setFont(new Font("Fixedsys", Font.PLAIN, 12));
-    antw1.setCursor(new Cursor(Cursor.HAND_CURSOR));
-    cp.add(antw1);
-    aufgabe.setBounds(72, 48, 297, 65);
-    aufgabe.setBackground(Color.BLACK);
-    aufgabe.setFont(new Font("Fixedsys", Font.PLAIN, 12));
-    aufgabe.setForeground(Color.GREEN);
-    aufgabe.setEditable(false);
-    cp.add(aufgabe);
-    antwGroup.add(antw1);
-    antwGroup.add(antw2);
-    antwGroup.add(antw3);
-    antwGroup.add(antw4);
-    btnAbschicken.setBounds(240, 272, 137, 57);
-    btnAbschicken.setText("weiter");
-    btnAbschicken.setMargin(new Insets(2, 2, 2, 2));
-    btnAbschicken.addActionListener(new ActionListener() { 
-      public void actionPerformed(ActionEvent evt) { 
-        btnAbschicken_ActionPerformed(evt);
-      }
-    });
-    btnAbschicken.setBackground(Color.BLACK);
-    btnAbschicken.setBorder(new javax.swing.border.LineBorder(Color.RED, 2));
-    btnAbschicken.setFont(new Font("Fixedsys", Font.PLAIN, 12));
-    btnAbschicken.setForeground(Color.GREEN);
-    btnAbschicken.setCursor(new Cursor(Cursor.HAND_CURSOR));
-    cp.add(btnAbschicken);
-    cp.setBackground(Color.BLACK);
-    setzeQuizfragen();
-    
-    setVisible(true);
-  } // end of public WieZu
-  
-  
-  // Anfang Methoden
-  
-  private void setzeQuizfragen(){
-    richt = quizProp.getProperty("richt" + rndCnt);
-    aufgabe.setText(quizProp.getProperty("aufgabe" + rndCnt));
-    int a = 4 * (rndCnt - 1);
-    antw1.setText(quizProp.getProperty("antw"+ (1 + a)));
-    antw2.setText(quizProp.getProperty("antw"+ (2 + a)));
-    antw3.setText(quizProp.getProperty("antw"+ (3 + a)));
-    antw4.setText(quizProp.getProperty("antw"+ (4 + a)));
-    
-    antwGroup.clearSelection(); // den ausgewählten Button für die
-    // nächste Runde zurücksetzen
-    
-    
-  }
-  public void antw1_ActionPerformed(ActionEvent evt) {
-    auswahl = evt.getActionCommand();
-  } 
-  
-  public void antw2_ActionPerformed(ActionEvent evt) {
-    auswahl = evt.getActionCommand();
-  } // end of antw2_ActionPerformed
-  
-  public void antw3_ActionPerformed(ActionEvent evt) {
-    auswahl = evt.getActionCommand();
-  } // end of antw3_ActionPerformed
-  
-  public void antw4_ActionPerformed(ActionEvent evt) {
-    auswahl = evt.getActionCommand();
-  } // end of antw3_ActionPerformed
-  protected void btnAbschicken_ActionPerformed(ActionEvent evt) {
-    System.out.println("Erfolg Platzhalter"); 
-    
-    if (auswahl.equals(richt)) {
-      pktCnt++;
-      System.out.println("Niederlage Platzhalter");
-      //nächste
-    } // end of if
-    else {
-      pktCnt--;
-      System.out.println("dies");
-    } // end of if-else
-    
-    if (rndCnt == 2) {
-      if (pktCnt == 2) {
-        new MiniGamesWinScreen("ERFOLG");
-        this.erfolg = true; 
-      } // end of if
-      else {
-        new MiniGamesLoseScreen("MISSLUNGEN");
-        this.erfolg = false;
-      } // end of if-else
-      
-      this.versuche = 1;
-      this.fertig = true;
-       guiAusblenden();
-    } // end of if
-    rndCnt++;
-    setzeQuizfragen();
-    
-    
-  }
-  // Ende Methoden
-  
-  public static void main(String[] args) {
-    new WieZu("WieZu");
-  } // end of main
-  
-} // end of class WieZu
+	// Anfang Attribute
+	
+	private static String titel = "Quiz";
+	
+	// Dummy-Klasse
+	private class QuizLader extends AbstractLader {
+		public QuizLader() {
+			super("quiz");
+		}
+	};
+	private AbstractLader quizLader = new QuizLader();
+	private Properties quizProp = quizLader.getProps().get(0);
+
+	private ButtonGroup antwGroup = new ButtonGroup();
+	private JTextField aufgabe = new JTextField();
+	private JButton btnAbschicken = new JButton();
+	
+	private List<JRadioButton> antw = new ArrayList<JRadioButton>();
+	
+	private int richt;
+	private int pktCnt = 0;
+	private int rndCnt = 1;
+	private int rundenzahl = 0;
+
+	private int auswahl = 0;
+	
+	private GuiWinScreen guiWinScreen = new GuiWinScreen();
+	private GuiLoseScreen guiLoseScreen = new GuiLoseScreen();
+
+	// Ende Attribute
+
+	public WieZu(int schwierigkeit) {
+		// Frame-Initialisierung
+		super(titel);
+		
+		int frameWidth = 457;
+		int frameHeight = 386;
+		setSize(frameWidth, frameHeight);
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (d.width - getSize().width) / 2;
+		int y = (d.height - getSize().height) / 2;
+		setLocation(x, y);
+		setResizable(false);
+		Container cp = getContentPane();
+		cp.setLayout(null);
+		// Anfang Komponenten
+
+		for(int i = 0; i < 4; i++) {
+			antw.add(new JRadioButton());
+			antw.get(i).setText("");
+			antw.get(i).setActionCommand(""+i);
+			antw.get(i).setOpaque(false);
+			antw.get(i).addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent evt) {
+					antw_ActionPerformed(evt);
+				}
+			});
+			antw.get(i).setBackground(Color.BLACK);
+			antw.get(i).setForeground(Color.GREEN);
+			antw.get(i).setFont(new Font("Fixedsys", Font.PLAIN, 12));
+			antw.get(i).setCursor(new Cursor(Cursor.HAND_CURSOR));
+			cp.add(antw.get(i));
+			antwGroup.add(antw.get(i));
+		}
+		
+		antw.get(0).setBounds(56, 128, 233, 41);
+		antw.get(1).setBounds(56, 176, 233, 41);
+		antw.get(2).setBounds(56, 224, 233, 41);
+		antw.get(3).setBounds(56, 272, 184, 41);
+		
+		aufgabe.setBounds(72, 48, 297, 65);
+		aufgabe.setBackground(Color.BLACK);
+		aufgabe.setFont(new Font("Fixedsys", Font.PLAIN, 12));
+		aufgabe.setForeground(Color.GREEN);
+		aufgabe.setEditable(false);
+		cp.add(aufgabe);
+		btnAbschicken.setOpaque(true);
+		btnAbschicken.setBounds(240, 272, 137, 57);
+		btnAbschicken.setText("weiter");
+		btnAbschicken.setMargin(new Insets(2, 2, 2, 2));
+		btnAbschicken.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				btnAbschicken_ActionPerformed(evt);
+			}
+		});
+		btnAbschicken.setBackground(Color.BLACK);
+		btnAbschicken
+				.setBorder(new javax.swing.border.LineBorder(Color.RED, 2));
+		btnAbschicken.setFont(new Font("Fixedsys", Font.PLAIN, 12));
+		btnAbschicken.setForeground(Color.GREEN);
+		btnAbschicken.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		cp.add(btnAbschicken);
+		cp.setBackground(Color.BLACK);
+	}
+
+	private void setzeQuizfragen() {
+		richt = Integer.parseInt(quizProp.getProperty("richt" + rndCnt));
+		aufgabe.setText(quizProp.getProperty("aufgabe" + rndCnt));
+		
+		int a = 4 * (rndCnt - 1) + 1;
+		for(int i = 0; i < 4; i++) {
+			antw.get(i).setText(quizProp.getProperty("antw" + ( i + a )));
+		}
+
+		// den ausgewählten Button für die nächste Runde zurücksetzen
+		antwGroup.clearSelection();
+	}
+
+	public void antw_ActionPerformed(ActionEvent evt) {
+		auswahl = Integer.parseInt(evt.getActionCommand());
+	}
+
+	private void btnAbschicken_ActionPerformed(ActionEvent evt) {
+		if(auswahl == (richt-(rndCnt-1)*4)-1) {
+			pktCnt++;
+		} else {
+			pktCnt--;
+		}
+
+		if (rndCnt == rundenzahl) {
+			if (pktCnt == rundenzahl) { // TODO: an Schwierigkeit anpassen
+				guiWinScreen.setVisible(true);
+				this.erfolg = true;
+			} else {
+				guiLoseScreen.setVisible(true);
+				this.erfolg = false;
+			}
+
+			this.versuche = 1;
+			this.fertig = true;
+		} else {
+			rndCnt++;
+			setzeQuizfragen();
+		}
+	}
+
+	public static void main(String[] args) {
+		AbstractMiniGame wieZu = new WieZu(0);
+		wieZu.initialisieren();
+		wieZu.setVisible(true);
+	} // end of main
+	
+	public void initialisieren() {
+		// die Anzahl an Aufgaben ermitteln
+		int i = 0;
+		do {
+			i++;
+		} while(quizProp.containsKey("aufgabe"+(i+1)));
+		rundenzahl = i;
+		
+		rndCnt = 1;
+		pktCnt = 0;
+		auswahl = 0;
+		versuche = 0;
+		fertig = false;
+		erfolg = false;
+		
+		setzeQuizfragen();
+	}
+	
+	public void setVisible(boolean b) {
+		// Die Methode "setVisible" der Mutterklasse wird überschrieben,
+		// damit auch die anderen Fenster geschlossen werden
+		if(b == false) {
+			guiLoseScreen.setVisible(false);
+			guiWinScreen.setVisible(false);
+		}
+		
+		// die eigentliche Funktion (die Überschrieben wurde), aufrufen.
+		super.setVisible(b);
+	}
+}
